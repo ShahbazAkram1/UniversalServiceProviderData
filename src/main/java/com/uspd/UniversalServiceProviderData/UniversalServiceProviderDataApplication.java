b553831f0f7fb3a0a10fbdffe813927313b255ff
Shahbaz Khan <shahbazkhaniq@gmail.com>
2023-09-10 22:45:06 +0500

    Initial commit

---
+++ src/main/java/com/uspd/UniversalServiceProviderData/UniversalServiceProviderDataApplication.java
@@ -0,0 +1,13 @@
+package com.uspd.UniversalServiceProviderData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalServiceProviderDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalServiceProviderDataApplication.class, args);
+	}
+
+}
